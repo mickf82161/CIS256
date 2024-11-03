f5bc8701d8247666b1f2dbd7417c7f6d573ece1c
mlillis87 <38544136+mlillis87@users.noreply.github.com>
2024-11-02 22:51:56 -0700

    initialize repo

---
+++ CIS156_17_State_Capitals_Quiz_Lillis.py
@@ -0,0 +1,75 @@
+import random
+
+#Functions
+def main():
+    setupGame()
+    playGame()
+
+def setupGame():
+    global states, capitals, correct, incorrect, used #declare globals to be used in other parts of program
+    correct = 0 #keep track of correct responses
+    incorrect = 0 #keep track of incorrect responses
+    used = [False] * 50 #keep track of whether we already asked that question
+    states = populateStatesList() #fill list using function
+    capitals = populateDict() #fill dict using function
+
+def playGame():
+    global correct, incorrect, used
+    guess = ""
+    while guess.lower() != "quit": #gameplay loop ends by typing quit
+        index = random.randint(0, len(states) - 1) #random index out of states list
+        while used[index]: #while the value inside the list is True
+            index = random.randint(0, len(states) - 1) #pick an index for a question we havent asked
+        used[index] = True #update list to true before asking question
+        allTrue = True
+        for i in range(0, 50): 
+            if used[i] == False:
+                allTrue = False
+        if allTrue: #if all questions have been asked, reset the game
+            used = [False] * 50
+        state = states[index] #grab state using random index
+        capital = capitals[state] #grab capital out of dict using matching state
+        guess = input("What is the capital of " + state.upper() + "? (enter 'quit' to end): ")
+        if guess.lower() == "quit": #check for sentinel
+            print("THANKS FOR PLAYING...You got " + str(correct) + " of " + str(correct + incorrect) + " correct.")
+            break
+        elif guess.lower() == capital.lower(): #correct response
+            print("That is CORRECT!" + capital + " is the capital of " + state + ".")
+            correct += 1
+        else: #incorrect response
+            print("SORRY...the capital of " + state + " is " + capital + ".")
+            incorrect += 1
+        print("YOUR SCORE: You have gotten " + str(correct) + " of " + str(correct + incorrect) + " correct.")
+    
+
+#List of states
+def populateStatesList():
+    return ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut',
+            'Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa',
+            'Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan',
+            'Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada',
+            'New Hampshire','New Jersey','New Mexico','New York','North Carolina',
+            'North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island',
+            'South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia',
+            'Washington','West Virginia','Wisconsin','Wyoming']
+
+#dictionary of states:capitals 
+def populateDict():
+    return {'Alabama':'Montgomery','Alaska':'Juneau','Arizona':'Phoenix','Arkansas':'Little Rock',
+            'California':'Sacramento','Colorado':'Denver','Connecticut':'Hartford',
+            'Delaware':'Dover','Florida':'Tallahassee','Georgia':'Atlanta','Hawaii':'Honolulu',
+            'Idaho':'Boise','Illinois':'Springfield','Indiana':'Indianapolis','Iowa':'Des Moines',
+            'Kansas':'Topeka','Kentucky':'Frankfort','Louisiana':'Baton Rouge','Maine':'Augusta',
+            'Maryland':'Annapolis','Massachusetts':'Boston','Michigan':'Lansing',
+            'Minnesota':'St. Paul','Mississippi':'Jackson','Missouri':'Jefferson City',
+            'Montana':'Helena','Nebraska':'Lincoln','Nevada':'Carson City','New Hampshire':'Concord',
+            'New Jersey':'Trenton','New Mexico':'Santa Fe','New York':'Albany',
+            'North Carolina':'Raleigh','North Dakota':'Bismarck','Ohio':'Columbus',
+            'Oklahoma':'Oklahoma City','Oregon':'Salem','Pennsylvania':'Harrisburg',
+            'Rhode Island':'Providence','South Carolina':'Columbia','South Dakota':'Pierre',
+            'Tennessee':'Nashville','Texas':'Austin','Utah':'Salt Lake City','Vermont':'Montpelier',
+            'Virginia':'Richmond','Washington':'Olympia','West Virginia':'Charleston',
+            'Wisconsin':'Madison','Wyoming':'Cheyenne'}
+
+#Run game
+main()
---
+++ README.md
@@ -0,0 +1,34 @@
+# US State Capitals Quiz
+
+This project is a Python-based quiz game to help users learn and test their knowledge of US state capitals. This game randomly selects states and prompts the user to input the corresponding capital. The program tracks correct and incorrect answers and provides a "quit" option to end the game.
+
+## Table of Contents
+- [Overview](#overview)
+- [Features](#features)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Overview
+This quiz is designed to improve your knowledge of U.S. state capitals. By randomly selecting states and prompting the user to answer, it creates an engaging and educational experience. The program tracks the number of correct and incorrect responses throughout the session.
+
+## Features
+- Randomly selects a state for each question.
+- Tracks the number of correct and incorrect answers.
+- Allows users to type "quit" to end the game at any time.
+- Prevents repeat questions within a single session.
+
+## Getting Started
+### Prerequisites
+- Python 3.x
+
+### Installation
+1. Download or clone this repository.
+2. Make sure you have Python installed on your system.
+
+## Usage
+1. Open the python script
+2. The program will begin by asking you to name the capital of a randomly selected state.
+3. Type your anser and press Enter. The program will track whether your answer is correct or incorrect.
+4. To end the game, type 'quit' and press enter.
